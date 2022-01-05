470a72723989fbd7ffa000a8e7d31bf239554323
ravikiranpanchaneni <ravi.panchaneni@gmail.com>
2022-01-05 22:10:59 +0530

    removed hard coded keys

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -4,18 +4,12 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const config = {
-  // apiKey: process.env.REACT_APP_API_KEY,
-  // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_APP_ID,
-  apiKey: "AIzaSyCnB1P3Kg-DWj8fvub5E-6CPZ4TnDtuLm4",
-  authDomain: "fir-react1-ca8d7.firebaseapp.com",
-  projectId: "fir-react1-ca8d7",
-  storageBucket: "fir-react1-ca8d7.appspot.com",
-  messagingSenderId: "426209960912",
-  appId: "1:426209960912:web:e68575bf5c62125e850fd5",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 if (!firebase.apps.length) {
