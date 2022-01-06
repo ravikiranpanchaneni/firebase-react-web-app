bd5820eac41859d20ee3478d1ae17c1942679255
ravikiranpanchaneni <ravi.panchaneni@gmail.com>
2022-01-06 22:44:41 +0530

    supressed eslint warning

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
       .finally(() => {
         setIsLoading(false);
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user]);
 
   FirebaseAuthService.subscribeToAuthChanges(setUser);
